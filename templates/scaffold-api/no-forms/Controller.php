99713d13869d7bb76b6ca875373b230128cf3c0f
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2019-05-11 01:42:39 +0430

    [CREATE] - empty template file for api controller

---
+++ templates/scaffold-api/no-forms/Controller.php
@@ -0,0 +1,11 @@
+<?php
+$namespace$
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+$useFullyQualifiedModelName$
+
+class $className$Controller extends ControllerBase
+{
+
+
+}
